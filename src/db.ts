16a6bf851cf4c082c550fb5ed859fa8fddf1e40c
rawadhossain <rawad.hossain00@gmail.com>
2024-11-24 12:53:08 +0600

    db schema

--- src/db.ts
+++ src/db.ts
@@ -16,14 +16,14 @@ const UserSchema = new Schema({
 
 export const UserModel = model('User', UserSchema); //Creates a model named User based on the UserSchema
 
-const ContentSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    link: String,
+const contentTypes = ['image', 'video', 'article', 'audio']; // Extend as needed
+
+const contentSchema = new Schema({
+    link: { type: String, required: true },
+    type: { type: String, enum: contentTypes, required: true },
+    title: { type: String, required: true },
     tags: [{ type: mongoose.Types.ObjectId, ref: 'Tag' }],
     userId: { type: mongoose.Types.ObjectId, ref: 'User', required: true },
 });
 
-export const ContentModel = model('Content', ContentSchema);
+export const ContentModel = model('Content', contentSchema);
